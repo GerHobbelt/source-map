def6a7d1705f4cfcc6e2fee6e6e1377a02f010e4
Mike Morearty <mike@asana.com>
2017-09-27 13:56:12 -0700

    quick-sort: Use the real Math.random(), never a mocked version of it
    The following scenario can occur in code that uses the source-map code:
    
    * A developer writes a test that needs to mock out Math.random() to
      return a constant value.
    * Their test fails, so their testing framework tries to capture a stack
      trace.
    * In the process of capturing the stack trace, a source map is loaded,
      which involves invoking quick-sort.
    * But quick-sort uses the still-mocked Math.random(), causing it to take
      a long time and then crash with a stack overflow.
    
    To help developers avoid this scenario, our quick-sort keeps a reference
    to the original Math.random function when it loads, and uses that during
    sorting.
    
    # Conflicts:
    #	dist/source-map.debug.js
    #	dist/source-map.js
    #	dist/source-map.min.js
    #	dist/source-map.min.js.map
    #	dist/test/test_api.js
    #	dist/test/test_dog_fooding.js
    #	dist/test/test_source_map_consumer.js
    #	dist/test/test_source_map_generator.js
    #	dist/test/test_source_node.js

--- test/test-quick-sort.js
+++ test/test-quick-sort.js
@@ -43,3 +43,34 @@ exports['test sorting unsorted array'] = function (assert) {
   assert.equal(JSON.stringify(ary.sort(numberCompare)),
                JSON.stringify(quickSorted));
 };
+
+exports['test sorting when Math.random has been mocked'] = function (assert) {
+  // Our dataset needs to be big enough to cause a stack overflow in quickSort
+  // if it invokes a mocked version of Math.random() that always returns zero.
+  var ary = [];
+  for (var i = 0; i < 10000; i++) {
+    ary.push(i);
+  }
+
+  // mock out Math.random()
+  var originalMathRandom = Math.random;
+  var usedMockedMathRandom = false;
+  Math.random = function() {
+    usedMockedMathRandom = true;
+    return 0;
+  };
+
+  var caught = null;
+  try {
+    quickSort(ary, numberCompare);
+  } catch (e) {
+    caught = e; // if we get here, it was probably a stack overflow
+  }
+
+  // clean up
+  Math.random = originalMathRandom;
+
+  assert.equal(usedMockedMathRandom, false);
+  assert.equal(caught, null);
+};
+
